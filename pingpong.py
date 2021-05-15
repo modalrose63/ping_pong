b4a165e74369f2c467749d6b44263931b1090526
modalrose63 <83130361+modalrose63@users.noreply.github.com>
2021-05-15 19:33:33 +0300

    added roketki and ball

--- pingpong.py
+++ pingpong.py
@@ -1,20 +1,19 @@
 from pygame import *
 from random import randint
 
-'''window = display.set_mode((1080,720))
+window = display.set_mode((1080,720))
 display.set_caption("Пингпонг")
-
-background = transform.scale(image.load("castle.png"), (1080,720))
-
+background = transform.scale(image.load("background_material.jpg"), (1080,720))
 keys_pressed = key.get_pressed()
-
 clock = time.Clock()
 FPS = 60
-
 font.init()
-font = font.SysFont('Times New Roman', 40)
-text = font.render("Ты проиграл!", True, (255,255,255))'''
-   
+font = font.SysFont('Arial', 40)
+text = font.render("Ты проиграл!", True, (255,255,255))
+
+speed_x = 3
+speed_y = 3
+
 class Gamesprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_hight):
         super().__init__()
@@ -28,29 +27,17 @@ class Gamesprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+ball = Gamesprite("circle_PNG71.png", 500, 500, 10, 70, 70)
 
 class Player(Gamesprite):
     def update(self):
 
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
 
-        if keys_pressed[K_d] and self.rect.x < 1030:
-            self.rect.x += self.speed
-
-
-class Enemy(Gamesprite):
-    def update(self):
-        global lost
-        if self.rect.y <= 700:
+        if keys_pressed[K_s] and self.rect.y < 570:
             self.rect.y += self.speed
-        else:
-            self.rect.y = randint(-500,0)
-            self.rect.x = randint(0,1000)
-            lost += 1
-
-
-
+plr1 = Player("pngwing.com.png", 25, 300, 10, 75, 150)
 
 finish = False
 game = True
@@ -59,6 +46,15 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+            
     if finish != True:
-        display.update()
+        window.blit(background,(0,0))
+        window.blit(ball,(500,500))
+        keys_pressed = key.get_pressed()
+        plr1.update()
+        plr1.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        clock.tick(FPS)
+
+        display.update() 
